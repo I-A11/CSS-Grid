be2b6e7a39754738651d0493945856e8c3eb847a
I-A11 <d.islam1983@gmail.com>
2021-08-18 12:17:36 +1000

    create filter function to filter the products

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const products = document.querySelector(".products");
+const products = document.querySelectorAll(".product-details");
 const search = document.getElementById("search");
 const productName = document.querySelectorAll(".product-details h3");
-
+const filters = document.querySelectorAll(".filter-products");
+// search
 search.addEventListener("keyup", (e) => {
   e.preventDefault();
   const text = e.target.value.toLowerCase();
@@ -10,9 +11,29 @@ search.addEventListener("keyup", (e) => {
     // console.log(item);
     if (item.toLowerCase().indexOf(text) != -1) {
       //   console.log(product.parentElement);
-      product.parentElement.style.display = "block";
+      product.parentElement.style.display = "grid";
     } else {
       product.parentElement.style.display = "none";
     }
   });
 });
+
+// filter
+for (let i = 0; i < filters.length; i++) {
+  filters[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    const filter = e.target.value;
+    // console.log(filter);
+    products.forEach((product) => {
+      if (filter == "all") {
+        product.style.display = "grid";
+      } else {
+        if (product.classList.contains(filter)) {
+          product.style.display = "grid";
+        } else {
+          product.style.display = "none";
+        }
+      }
+    });
+  });
+}
